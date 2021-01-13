a4ac6ea96bb70cf5dcdbe0c35c8fce08619a4f85
oksanalyakhova <o.v.lyakhova@gmail.com>
2021-01-13 18:11:43 +0200

    removed webmanifest

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/js/sw.js')
-      .then(reg => console.log('SW registered!', reg))
-      .catch(err => console.log('Boo!', err));
-      })
-  }
